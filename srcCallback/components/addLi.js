6ec79fa9819c3a5e7e81b3a5487c3462651b77b8
rafael garcia <rago89@gmail.com>
2021-07-28 12:22:37 +0200

    fix umeration add lap

--- srcCallback/components/addLi.js
+++ srcCallback/components/addLi.js
@@ -1,5 +1,6 @@
 export const addLiComponent = (obj) => {
   const liEl = document.createElement("li");
+  obj.counter++;
   const { hours, milliseconds, minutes, seconds } = obj.time;
   liEl.innerHTML = `${obj.counter}. ${hours}:${minutes}:${seconds}.${milliseconds}`;
   return liEl;
